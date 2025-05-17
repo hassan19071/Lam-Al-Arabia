1ef6defb6695df1d8f24c802ce192c5e9369a711
Hassan <hk5949467@gmail.com>
2025-05-17 14:06:35 +0300

    some updates

--- src/components/FooterAr.js
+++ src/components/FooterAr.js
@@ -7,15 +7,33 @@ function FooterAr() {
       <div className="container px-lg-5">
         <div className="footer-text d-flex justify-content-between">
           <div className="copyright">&copy;2025 جميع الحقوق محفوظة</div>
-          <div className="social-media">
-            <div className="icons">
-              <Link to="#">
-                <i className="fa-brands fa-facebook-f"></i>
+          <div className="social-icons d-flex align-items-center">
+            <div className="insta">
+              <Link
+                target="_blank"
+                to="https://www.instagram.com/lam_customers?igsh=Z243eXA1bDg5dXl5"
+              >
+                <i className="fa-brands fa-instagram"></i>
               </Link>
-              <Link to="#">
+            </div>
+            <div className="linkedin mx-2">
+              <Link
+                target="_blank"
+                to="http://www.linkedin.com/in/advertising-services-73b935355"
+              >
                 <i className="fa-brands fa-linkedin-in"></i>
               </Link>
-              <Link to="https://wa.me/966593072004" target="_blank">
+            </div>
+            <div className="x">
+              <Link
+                target="_blank"
+                to="https://x.com/lam_customers?t=v7s27b6qztSJG547PhYwEg&s=09"
+              >
+                <i className="fa-brands fa-x"></i>
+              </Link>
+            </div>
+            <div className="whatsapp me-2">
+              <Link target="_blank" to="https://wa.me/966593072004">
                 <i className="fa-brands fa-whatsapp"></i>
               </Link>
             </div>
